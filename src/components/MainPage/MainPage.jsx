9f2124281b06f1b8cbf9adfb8faceab797d1834a
Viktor Kindrat <kindratvictor07@gmail.com>
2023-04-24 17:15:39 +0300

    Main page component add

---
+++ src/components/MainPage/MainPage.jsx
@@ -0,0 +1,11 @@
+import "./MainPage.css"
+
+function MainPage() {
+    return(
+        <section className="MainPage">
+            <span>Main page in development 🛠️</span>
+        </section>
+    )
+}
+
+export default MainPage
