e643e92a59fee38abd4600307d35e682a5ee2047
yuriy-kulakovskyi <yurakulak42@gmail.com>
2023-04-24 21:51:28 +0300

    space between comments

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
   return (
     <div className="App">
       {/* The place to hold components */}
+      
       {/* Header component */}
       <Header/>
       {/* MainPage component */}
