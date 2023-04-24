7f7d88f162f4dd20e695caf3cd39987e1620c7f4
Viktor Kindrat <kindratvictor07@gmail.com>
2023-04-24 20:39:13 +0300

    Update main slider. Customize pagination. Upload images for mobiles

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -8,19 +8,23 @@ register();
 const slidesData = [
     {
         id: 1,
-        image: "./MainPage/slide1.webp"
+        image: "./MainPage/slide1.webp",
+        imageForPhones: "./MainPage/slide1-phone.webp"
     },
     {
         id: 2,
-        image: "./MainPage/slide2.webp"
+        image: "./MainPage/slide2.webp",
+        imageForPhones: "./MainPage/slide2-phone.webp"
     },
     {
         id: 3,
-        image: "./MainPage/slide3.webp"
+        image: "./MainPage/slide3.webp",
+        imageForPhones: "./MainPage/slide3-phone.webp"
     },
     {
         id: 4,
-        image: "./MainPage/slide4.webp"
+        image: "./MainPage/slide4.webp",
+        imageForPhones: "./MainPage/slide4-phone.webp"
     },
 ]
 
@@ -33,28 +37,38 @@ function MainPage() {
                     simulate-touch="false"
                     autoplay-delay="5000"
                     direction="horizontal"
-                    navigation="true"
-                    pagination="true"
                     slides-per-view="1"
                     speed="500"
                     loop="true"
+                    navigation="true"
                     navigation-next-el=".MainPage-slider-btn-next"
-                    navigation-prev-el=".MainPage-slider-btn-previous">
-                        <div className="MainPage-slider-btn MainPage-slider-btn-previous">
-                            <img className="MainPage-slider-btn-svg" src={prevSvg} alt="previous slide button" />
-                        </div>
-                        {
-                            slidesData.map(item =>
-                                <swiper-slide key={item.id}>
-                                    <div className="MainPage-slide-inner">
-                                        <img src={item.image} alt={`Slide${item.id}`} />
-                                    </div>
-                                </swiper-slide>
-                            )
-                        }
-                        <div className="MainPage-slider-btn MainPage-slider-btn-next">
-                            <img className="MainPage-slider-btn-svg" src={nextSvg} alt="next slide button" />
-                        </div>
+                    navigation-prev-el=".MainPage-slider-btn-previous"
+                    pagination-el=".MainPage-slider-pagination"
+                    pagination-bullet-element="span"
+                    pagination-bullet-class="MainPage-slider-pagination-bullet"
+                    pagination-bullet-active-class="MainPage-slider-pagination-bullet-active"
+                    pagination-bullet-current-class="MainPage-slider-pagination-bullet"
+                    pagination-clickable="true"
+                >
+                    <div className="MainPage-slider-pagination">
+                        <span className="MainPage-slider-pagination-bullet-active"></span>
+                        <span className="MainPage-slider-pagination-bullet"></span>
+                    </div>
+                    <div className="MainPage-slider-btn MainPage-slider-btn-previous">
+                        <img className="MainPage-slider-btn-svg" src={prevSvg} alt="previous slide button" />
+                    </div>
+                    {
+                        slidesData.map(item =>
+                            <swiper-slide key={item.id}>
+                                <div className="MainPage-slide-inner">
+                                    <img src={item.image} alt={`Slide${item.id}`} />
+                                </div>
+                            </swiper-slide>
+                        )
+                    }
+                    <div className="MainPage-slider-btn MainPage-slider-btn-next">
+                        <img className="MainPage-slider-btn-svg" src={nextSvg} alt="next slide button" />
+                    </div>
                 </swiper-container>
             </div>
         </section>
