8e99b4887cb754a9d4d29110a5139cde4a79d3b2
Paravojik <muhaloiko.vitalik@gmail.com>
2023-04-25 20:12:11 +0300

    added burger with animation and adaptive

---
+++ src/components/Header/HeaderBurger.jsx
@@ -0,0 +1,12 @@
+import './Header.css'
+
+function HeaderBurger(){
+    return(
+        <div className='headerBurger'>
+            <div className="headerBurger__row"></div>
+            <div className="headerBurger__row"></div>
+            <div className="headerBurger__row"></div>
+        </div>
+    )
+}
+export default HeaderBurger
