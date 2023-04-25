ad61ac95fa6235d43c97fa5cc2fd0147d303496c
Viktor Kindrat <kindratvictor07@gmail.com>
2023-04-25 18:54:46 +0300

    HelpSlider component created. Images uploaded

---
+++ src/components/HelpSlider/HelpSlider.jsx
@@ -0,0 +1,16 @@
+import { register } from 'swiper/element/bundle';
+
+import "./HelpSlider.css"
+import sliderData from "./slides.json"
+
+register();
+
+function HelpSlider (){
+    return (
+        <section className="HelpSlider">
+
+        </section>
+    )
+}
+
+export default HelpSlider
