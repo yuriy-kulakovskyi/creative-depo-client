d8c54d6fa1c32ae47653d1b668cacfd096057e1f
Viktor Kindrat <kindratvictor07@gmail.com>
2023-04-25 17:56:07 +0300

    add event listeners on slide click. bug fixes

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,41 +1,31 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { register } from 'swiper/element/bundle';
 
 import nextSvg from "./images/next.svg"
 import prevSvg from "./images/previous.svg"
+import slidesData from "./slides.json"
 import "./MainPage.css"
 
 register();
-const slidesData = [
-    {
-        id: 1,
-        image: "./MainPage/slide1.webp",
-        imageForPhones: "./MainPage/slide1-phone.webp"
-    },
-    {
-        id: 2,
-        image: "./MainPage/slide2.webp",
-        imageForPhones: "./MainPage/slide2-phone.webp"
-    },
-    {
-        id: 3,
-        image: "./MainPage/slide3.webp",
-        imageForPhones: "./MainPage/slide3-phone.webp"
-    },
-    {
-        id: 4,
-        image: "./MainPage/slide4.webp",
-        imageForPhones: "./MainPage/slide4-phone.webp"
-    },
-]
 
 function MainPage() {
-    let [needToRz, setNeedToRz] = useState(!(window.innerWidth >= 800))
-    useEffect(()=>{
-        window.addEventListener("resize", function(){
-            setNeedToRz(!(window.innerWidth >= 800))
-        })
+    let [screenWidth, setScreenWidth] = useState(window.innerWidth);
+    let setAdaptivityImage = useCallback((item) => (screenWidth >= 800) ? item.image : item.imageForPhones, [screenWidth])
+    useEffect(() => {
+        let callback = () => setScreenWidth(window.innerWidth)
+        window.addEventListener("resize", callback)
+        return () => {
+            window.removeEventListener("resize", callback)
+        }
     }, [])
+
+    let moveTo = useCallback(function (selector) {
+        if (selector) {
+            let element = document.querySelector(selector);
+            element.scrollIntoView({ behavior: "smooth", block: "start" })
+        }
+    }, [])
+
     return (
         <section className="MainPage">
             <div className="MainPage-slider-container">
@@ -58,32 +48,21 @@ function MainPage() {
                     pagination-clickable="true"
                 >
                     <div className="MainPage-slider-pagination">
-                        <span className="MainPage-slider-pagination-bullet-active"></span>
-                        <span className="MainPage-slider-pagination-bullet"></span>
+                        <span className="MainPage-slider-pagination-bullet-active"></span><span className="MainPage-slider-pagination-bullet"></span>
                     </div>
                     <div className="MainPage-slider-btn MainPage-slider-btn-previous">
                         <img className="MainPage-slider-btn-svg" src={prevSvg} alt="previous slide button" />
                     </div>
                     {
-                        function(){
-                            if (window.innerWidth >= 800) {
-                                return slidesData.map(item =>
-                                    <swiper-slide key={item.id}>
-                                        <div className="MainPage-slide-inner">
-                                            <img src={item.image} alt={`Slide${item.id}`} />
-                                        </div>
-                                    </swiper-slide>
-                                )
-                            } else {
-                                return slidesData.map(item =>
-                                    <swiper-slide key={item.id}>
-                                        <div className="MainPage-slide-inner">
-                                            <img src={item.imageForPhones} alt={`Slide${item.id}`} />
-                                        </div>
-                                    </swiper-slide>
-                                )
-                            }
-                        }()
+                        slidesData.map(item =>
+                            <swiper-slide key={item.id}>
+                                <div className="MainPage-slide-inner">
+                                    <button className="MainPage-slideBtn" onClick={() => moveTo(`${item.action}`)}>
+                                        <img src={setAdaptivityImage(item)} alt={`Slide${item.id}`} />
+                                    </button>
+                                </div>
+                            </swiper-slide>
+                        )
                     }
                     <div className="MainPage-slider-btn MainPage-slider-btn-next">
                         <img className="MainPage-slider-btn-svg" src={nextSvg} alt="next slide button" />
