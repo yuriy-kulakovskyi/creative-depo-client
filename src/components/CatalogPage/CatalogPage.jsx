28ad9b21f3d965525a906567d6ae593502110e33
Oksana Moskviak <84397218+okqsna@users.noreply.github.com>
2023-04-27 17:34:18 +0300

    updated filter algorithm

--- src/components/CatalogPage/CatalogPage.jsx
+++ src/components/CatalogPage/CatalogPage.jsx
@@ -6,36 +6,42 @@ import './CatalogPage.css';
 function CatalogPage(){
     const [isPending, startTransition] = useTransition();
     const [receivedData, setReceivedData]=useState([]);
+    const [Categories, setCategories]=useState([]);
     const [selectedCategory, setSelectedCategory]=useState('Всі');
     const [loading, setLoading]=useState(true);
     const [error, setError]=useState(null);
-    const Categories=["Всі", "футболки", "худі", "кепки", "аксесуари"];
     useEffect(()=>{
         fetch(`https://creative-depo.onrender.com/products`)
         .then(response=>response.json())
         .then(data=>{
             setReceivedData(data);
             setLoading(false);
+            const Categories=[...new Set(data.map((p)=>p.category)),];
+            Categories.unshift('Всі');
+            setCategories(Categories);
         })
         .catch((err)=>{
             setError(err);
             console.log(err.message);
         })
-
     }, [])
+
+
+
     function CategoryClick(categoryName){
         startTransition(() => {
             setSelectedCategory(categoryName);     
         });
     }
     function filterProducts(receivedData, category){
-        if(category===Categories[0]){
+        if(category==="Всі"){
             return receivedData;
         }else{
             return receivedData.filter((product)=>product.category===category);
         }
     }
     const filteredProducts=filterProducts(receivedData, selectedCategory);
+
 
     return(
         <section className="CatalogPage">
