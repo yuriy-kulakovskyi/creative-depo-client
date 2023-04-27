2edbe6baa37fc108a9ca17f51ce0d76077ef4f59
Oksana Moskviak <84397218+okqsna@users.noreply.github.com>
2023-04-27 15:31:53 +0300

    Delete src/components/CatalogPage directory

--- src/components/CatalogPage/CatalogPage/ProductBtn/ProductBtn.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import './ProductBtn.css';
-
-const ProductBtn=()=>{
-    return(
-        <button className="btn__bottom__product__ProductList">Додати у кошик</button>
-    )
-}
-export default ProductBtn;
