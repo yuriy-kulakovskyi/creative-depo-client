cdb4a284c6b94f2b79990260cad1f332a5bebfd1
Andriy_Step <andystep2008@gmail.com>
2023-04-29 23:03:22 +0300

    fixing bugs 4(popup)

--- src/components/PopUp/popup.jsx
+++ src/components/PopUp/popup.jsx
@@ -10,33 +10,40 @@ import jquery from 'jquery'
 import $ from 'jquery'
 
 function Popup (){
-
-
-    const [count, setCount] = useState(1)
-
-    let arr = []
-
-    useEffect(() => {
-
-        fetch('https://creative-depo.onrender.com/products')
-            .then((response) => {
-                return response.json();
-            })
-            .then((data) => {
-                arr = data
-                console.log(arr)
-            });
-
-        // $('.product__ProductList').click(function(e){
-        //     alert(e.target.id)
-        // });
-
-    })
-
     return(
-
-        <div className="popup-wrap"></div>
+        <div>Hello</div>
     )
 }
 
 export default Popup;
+// function Popup (){
+
+
+//     const [count, setCount] = useState(1)
+
+//     let arr = []
+
+//     useEffect(() => {
+
+//         fetch('https://creative-depo.onrender.com/products')
+//             .then((response) => {
+//                 return response.json();
+//             })
+//             .then((data) => {
+//                 arr = data
+//                 console.log(arr)
+//             });
+
+//         // $('.product__ProductList').click(function(e){
+//         //     alert(e.target.id)
+//         // });
+
+//     })
+
+//     return(
+
+//         <div className="popup-wrap"></div>
+//     )
+// }
+
+// export default Popup;
